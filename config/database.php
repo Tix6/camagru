9ee50ebb3d0955518a81f65a4645c801333e0cb5
Matthias LECONTE <mleconte@e1r12p4.42.fr>
2016-10-15 19:13:43 +0200

    Initfiles.

---
+++ config/database.php
@@ -0,0 +1,5 @@
+<?php
+$DB_DSN = 'mysql:dbname=db_camagru;host=127.0.0.1;port=3307';
+$DB_USER = 'root';
+$DB_PASSWORD = 'root';
+?>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Camagru</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+      <header>
+          <h1 class="title">Camagru</h1>
+      </header>
+      <section>
+          main container
+      </section>
+      <aside>
+          pictures pagination
+      </aside>
+      <footer>
+          footer de qualite
+      </footer>
+    </body>
+</html>

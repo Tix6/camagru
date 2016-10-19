ee1e6e96dd6ff705b43b12de78e17ea03fa135f6
Matthias LECONTE <mleconte@student.42.fr>
2016-10-19 21:39:01 +0200

    Fix issue with sqlite.

--- sql/create.php
+++ sql/create.php
@@ -5,46 +5,42 @@
 $TABLES = array(
     /* USER TABLE */
     'user' => "CREATE TABLE IF NOT EXISTS `User` (
-    `id` int NOT NULL,
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
     `name` varchar(255) NOT NULL,
     `passwd` varchar(255) NOT NULL,
     `mail` varchar(255) NOT NULL,
     `token` varchar(255) DEFAULT NULL,
     `level` tinyint DEFAULT 0,
-    PRIMARY KEY (`id`),
     UNIQUE (`mail`)
     );",
 
     /* PICTURE TABLE */
     'picture' => "CREATE TABLE IF NOT EXISTS `Picture` (
-    `id` int NOT NULL,
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
     `user_id` int NOT NULL,
     `path` varchar(255) NOT NULL,
     `likes` int DEFAULT 0,
     `creation` timestamp DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`),
     FOREIGN KEY (`user_id`) REFERENCES User(id),
     UNIQUE (`path`)
     );",
 
     /* COMMENT TABLE */
     'comment' => "CREATE TABLE IF NOT EXISTS `Comment` (
-    `id` int NOT NULL,
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
     `user_id` int NOT NULL,
     `user_name` varchar(255) NOT NULL,
     `picture_id` int NOT NULL,
     `comment` text NOT NULL,
-    PRIMARY KEY (`id`),
     FOREIGN KEY (`user_id`) REFERENCES User(id),
     FOREIGN KEY (`picture_id`) REFERENCES Picture(id)
     );",
 
     /* LIKE TABLE */
     'like' => "CREATE TABLE IF NOT EXISTS `Like` (
-    `id` int NOT NULL,
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
     `user_id` int NOT NULL,
     `picture_id` int NOT NULL,
-    PRIMARY KEY (`id`),
     FOREIGN KEY (`user_id`) REFERENCES User(id),
     FOREIGN KEY (`picture_id`) REFERENCES Picture(id)
     );"
